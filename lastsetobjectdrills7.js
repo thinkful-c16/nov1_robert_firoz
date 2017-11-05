3c7480582a1afc1e5c0d076aa1e3b244f0606627
Firoz Kamdar <firozkamdar@gmail.com>
2017-11-04 18:30:15 -0700

    confirmed first method working

--- lastsetobjectdrills7.js
+++ lastsetobjectdrills7.js
@@ -1,38 +1,4 @@
 'use strict';
-// const characters = [];
-
-// function createCharacter(name, nickName, race, origin, attack, defense){
-//   let x = 0;
-//   let y = 0;
-//   return {
-//     name: '',
-//     nickName: '',
-//     race: '',
-//     origin: '',
-//     attack: 0,
-//     defense: 0,
-//     describe: function() {
-//       console.log(`${name} is a ${race} from ${origin}.`);
-//     },
-//     evaluatFight: function(character){
-//       if (character.defense >= character.attack) {
-//         y = 0;
-//       } else {
-//         y = character.attack - character.defense;
-//       }
-//       if (character.attack >= character.defense) {
-//         x = 0;
-//       } else {
-//         x = character.defense - character.attack;
-//       }
-//       console.log(`Your opponent takes ${x} damage and you receive ${y} damage.`);
-//     }   
-//   }
-// } // closing bracket for createCharacter function
-
-// createCharacter('Gandalf the white', 'gandalf', 'Wizard', 'Middle Earth', 10, 6 );
-// console.log(characters[0]);
-
 const characters = [];
 
 function createCharacter(name, nickName, race, origin, attack, defense){
@@ -42,12 +8,32 @@ function createCharacter(name, nickName, race, origin, attack, defense){
     race,
     origin,
     attack,
-    defense
-  });
+    defense,
+    'describe': function(){
+      return `
+      ${this.name} is a ${this.race} from ${this.origin}`;
+    },
+    // evaluateFight: function(character){
+    //   console.log(`Your opponent takes ${x} damage and you receive ${y} damage.`);
+    // },
+  }); return characters;
+}
+//populates empty array
+createCharacter('Gandalf the white', 'gandalf', 'Wizard', 'Middle Earth', 10, 6);
+createCharacter('Bilbo Baggins', 'bilbo', 'Hobbit', 'The Shire', 3, 2);
+createCharacter('Frodo Baggins', 'frodo', 'Hobbit', 'The Shire', 3, 2);
+createCharacter('Aragorn son of Arathorn', 'aragorn', 'Man', 'Dunedain', 6, 8);
+createCharacter('Legolas', 'legolas', 'Elf', 'WoodlandRealm', 8, 5, '','');
+//prints character object
+console.log(characters);
+
+//prints description of one character specified by index in array
+console.log(characters[0].describe());
+
+//prints description of all characters
+for (let key in characters){
+console.log(characters[key].describe());
 }
 
-createCharacter('Gandalf the white', 'gandalf', 'Wizard', 'Middle Earth', 10, 6 );
-createCharacter('Bilbo Baggins', 'bilbo', 'Hobbit', 'The Shire', 3, 2);
-console.log(characters);
 
 
